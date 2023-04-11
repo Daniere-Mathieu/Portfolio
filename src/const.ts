5a7e4688242094d9f71b06bc1fbed1a106a0de98
Mathieu-Daniere <mathieudaniere@orange.fr>
2023-04-11 08:47:07 +0200

    change text

--- src/const.ts
+++ src/const.ts
@@ -28,7 +28,7 @@ export const projects: list[] = [
     name: "Ereko",
     path: `${import.meta.env.BASE_URL}/assets/svg/ereko_logo.svg`,
     description:
-      "Le projet Ereko est un lecteur de musique qui recupere les musiques depuis youtube , les enregistre coter serveur et les redistribue au playlist qui ont besoin de cette musique",
+      "Ereko était un projet de cours où nous avons utilisé l'API de recherche YouTube pour récupérer les musiques les plus populaires, que nous avons enregistrées sur un serveur avant de les redistribuer aux playlists ciblées. Ce projet a été une opportunité de développer mes compétences en développement web et en intégration de services tiers.",
     githubLink: "https://github.com/Daniere-Mathieu/ereko",
     // link: "http://ereko.devcarotte.fr",
     alt: "logo de Ereko",
@@ -38,7 +38,7 @@ export const projects: list[] = [
     path: `${import.meta.env.BASE_URL}/assets/pronto.png`,
     link: "https://www.lepronto.fr",
     description:
-      "Le projet pronto est un projet que je réalise durant mon alternance, il s'agit d'un ERP pour les restaurateur qui leur permet de gerer leurs caisse ainsi que leurs ressources humaines.",
+      "Le projet Pronto est un ERP destiné aux restaurateurs, que j'ai développé en collaboration avec une équipe en tant que développeur en alternance. Ce projet m'a permis d'acquérir des compétences solides en développement d'applications métier et en travail d'équipe pour répondre aux besoins des clients. Je suis fier de contribuer à cette initiative qui apporte une réelle valeur ajoutée au secteur de la restauration.",
     alt: "logo de pronto",
   },
 ];
