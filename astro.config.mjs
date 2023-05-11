507380cb11cb2c29355ef0a5f793910015e73f0f
Mathieu-Daniere <mathieudaniere@orange.fr>
2023-05-11 10:18:55 +0200

    add animation and change some style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 
@@ -7,7 +7,12 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), sitemap(), image(), robotsTxt()],
-  base: '/Portfolio',
-  site: 'https://Daniere-Mathieu.github.io'
+  integrations: [
+    preact(),
+    sitemap(),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+    robotsTxt(),
+  ],
+  base: "/Portfolio",
+  site: "https://Daniere-Mathieu.github.io",
 });
